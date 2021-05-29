322447f12614e718923a2a927159fb0965d06e4a
ivanatomic43 <ivana.tomic96@gmail.com>
2021-05-29 15:56:18 +0200

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO USERS (id, email, first_name, last_name, password, phone_number) VALUES (1, 'ivana@gmail.com','Ivana', 'Tomic', 'sifra123', '0631234777');
+INSERT INTO USERS (id, email, first_name, last_name, password, phone_number) VALUES (2, 'miki@gmail.com','Miki', 'Tomic', 'sifra123', '0631234888');
